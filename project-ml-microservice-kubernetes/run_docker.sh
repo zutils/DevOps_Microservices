5de5235417cbfbaa8b6f461b1e7994853ce6969f
James Prince <34577138+zutils@users.noreply.github.com>
2021-03-11 17:17:37 -0500

    modified run docker script

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t project4 .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -p8000:80 -it --rm project4
